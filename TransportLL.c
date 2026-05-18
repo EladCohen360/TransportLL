aff474cf78437fa7096f1cd7c39132da2183c579
Elad Cohen <eladcohen360@gmail.com>
2026-05-18 18:53:19 +0300

    updated ver

--- TransportLL.c
+++ TransportLL.c
@@ -7,57 +7,124 @@
 
 TransportDB *TransportCreate(void)
 {
-   TransportDB *tdb = (TransportDB*)malloc(sizeof(TransportDB));
-   if(tdb==NULL)
-   {
-    return NULL;
-   }
+TransportDB *tdb = (TransportDB*)malloc(sizeof(TransportDB));
 
+    if(tdb==NULL)
+        return NULL;
+    
+    return tdb;
 }
 
 
 void TransportDestroy(TransportDB *tdb)
 {
-    while(tdb->next!=NULL)
+    if(tdb!=NULL)
     {
-        free(tdb)
+        
+        while(tdb->Lines!=NULL)
+        {
+            allLines *next_line = tdb->Lines->nextLine;
+
+            StationsList *station = tdb->Lines->stations;
+
+            while (station!= NULL)
+            {
+                StationsList *next_station = station->nextStation;
+                free(station->stationsName);
+                free(station);
+                station = next_station;
+            }
+            free(tdb->Lines);
+            tdb->Lines = next_line;
+            
+        }
+        free(tdb);
     }
+    return;
+    
+}
+
+
+TransportResult is_type_valid(allLines *new_line, const char *type)
+{
+    if(strcmp(type ,"BUS")==0)
+    {
+        new_line->type = BUS;
+    }
+    else if(strcmp(type ,"METRO")==0)
+    {
+        new_line->type = METRO;
+    }
+    else if(strcmp(type ,"TRAIN")==0)
+    {
+        new_line->type = TRAIN;
+    }
+    else
+    {   
+        free(new_line);
+        return TRANSPORT_INVALID_LINE_TYPE;
+    }
+    return TRANSPORT_SUCCESS;
+}
+
+
+TransportResult is_id_valid(allLines *current_line ,allLines *new_line, int line_id)
+{
+    while(current_line != NULL)
+    {
+        if(current_line->line_id==line_id)
+        {
+            free(new_line);
+            return TRANSPORT_ALREADY_EXISTS;
+        }
+        current_line=current_line->nextLine;
+    }
+    if(line_id<=0)
+        {
+            free(new_line);
+            return TRANSPORT_INVALID_LINE_NUMBER;
+        }
+}
+
+
+TransportResult is_price_valid(allLines *new_line, float price)
+{
+    if(price<=0)
+    {
+        free(new_line);
+        return TRANSPORT_INVALID_PRICE;
+    }
+    new_line->price=price;
 }
 
 
 TransportResult TransportAddLine(TransportDB* tdb, const char *type, int line_id, float price)
 {
+    if(tdb == NULL)
+        return TRANSPORT_NULL_ARGUMENTS;
+    
+    allLines *new_line=(allLines *)malloc(sizeof(allLines));
+    if(new_line==NULL)
+        return TRANSPORT_OUT_OF_MEMORY;
 
+    if(is_type_valid(new_line, type)==TRANSPORT_INVALID_LINE_TYPE)
+        return TRANSPORT_INVALID_LINE_TYPE;
 
+    allLines *current_line = tdb->Lines;
+    if(is_id_valid(current_line, new_line, line_id)!=TRANSPORT_ALREADY_EXISTS)
+        return  TRANSPORT_ALREADY_EXISTS;
+    if(is_id_valid(current_line, new_line, line_id)!=TRANSPORT_INVALID_LINE_NUMBER)
+        return  TRANSPORT_INVALID_LINE_NUMBER;
+     new_line->line_id=line_id;
+
+    if(is_price_valid(new_line, price)==TRANSPORT_INVALID_PRICE)
+        return TRANSPORT_INVALID_PRICE;
+
+    new_line->nextLine = tdb->Lines;
+    tdb->Lines= new_line;
+
+    return TRANSPORT_SUCCESS;
 }
-
-
-// TransportDB *add_line(char *tokens[], int count, TransportDB *allLines)
-// {
-//     lineDetails *newNode = malloc(sizeof(lineDetails));
-//     if (newNode == NULL)
-//     {
-//         prog2_report_error_message(TRANSPORT_OUT_OF_MEMORY);
-//         return allLines;
-//     }
-//     newNode->stations_list = NULL;
-//     if (strcmp(tokens[2], "BUS") == 0)
-//         newNode->type = BUS;
-//     else if (strcmp(tokens[2], "TRAIN") == 0)
-//         newNode->type = TRAIN;
-//     else if (strcmp(tokens[2], "METRO") == 0)
-//         newNode->type = METRO;
-//     else{
-//         prog2_report_error_message(TRANSPORT_INVALID_ARGUMENTS);
-//         free(newNode);
-//         return allLines;
-//     }
-//     newNode->line_id = atoi(tokens[3]);
-//     newNode->num_of_stations = atoi(tokens[4]);
-//     newNode->price = atof(tokens[5]);
-//     newNode->next = allLines;
-//     return newNode;
-// }
 
 
 TransportResult TransportRemoveLine(TransportDB* tdb, int line_id)
--- TransportLL.h
+++ TransportLL.h
@@ -1,27 +1,35 @@
-#ifndef __MOOVITLIST__
-#define __MOOVITLIST__
+#ifndef __TransportLL__
+#define __TransportLL__
 #include "prog2_ex2.h"
 
 typedef struct StationsList_t
 {
-    char *stations_list;
-    struct StationsList_t *next;
+    char *stationsName;
+    struct StationsList_t *nextStation;
 }StationsList;
 
 
-typedef struct TransportDB_t
+typedef struct allLines_t
 {
     int line_id;
     float price;
     TransportType type;
-    StationsList stations;
-    struct TransportDB_t *next;
-    
+    StationsList *stations;
+    struct allLines_t *nextLine;
+}allLines;
+
+
+typedef struct TransportDB_t
+{
+    allLines *Lines;
 }TransportDB;
 
 
 TransportDB *TransportCreate(void);
 void TransportDestroy(TransportDB *tdb);
+TransportResult is_type_valid(allLines *new_line, const char *type);
+TransportResult is_id_valid(allLines *current_line ,allLines *new_line, int line_id);
+TransportResult is_price_valid(allLines *new_line, float price);
 TransportResult TransportAddLine(TransportDB* tdb, const char *type, int line_id, float price);
 TransportResult TransportRemoveLine(TransportDB* tdb, int line_id);
 
