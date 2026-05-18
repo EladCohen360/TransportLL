f8b4ea27115187080d02bd552444021e598a5b32
Elad Cohen <eladcohen360@gmail.com>
2026-05-18 12:55:49 +0300

    updated ver

--- TransportLL.h
+++ TransportLL.h
@@ -2,33 +2,22 @@
 #define __MOOVITLIST__
 #include "prog2_ex2.h"
 
-typedef enum {
-    CMD_ADD_LINE,
-    CMD_REMOVE_LINE,
-    CMD_ADD_STATION_TO_LINE,
-    CMD_REPORT_LINES,
-    CMD_REPORT_STATIONS,
-    CMD_REPORT_DIRECTIONS,
-    CMD_COMMENT,
-    CMD_UNKNOWN
-} CommandType;
 
-
-typedef struct lineDetails_t
+typedef struct TransportDB_t
 {
     int line_id;
     float price;
-    transport_type type;
+    TransportType type;
     int num_of_stations;
     char *stations_list;
-    struct lineDetails_t *next;
+    struct TransportDB_t *next;
     
-}lineDetails;
+}TransportDB;
 
-void configure_io_from_args(int argc, char *argv[] ,FILE **in ,FILE **out);
-CommandType identify_command(char *tokens[], int count);
-lineDetails *add_line(char *tokens[], int count, lineDetails *allLines);
-void remove_line(char *tokens[], int count, lineDetails *allLines);
-void add_station_to_line(char *tokens[], int count, lineDetails *allLines);
+TransportDB *TransportCreate(void);
+void TransportDestroy(TransportDB *tdb);
+TransportResult TransportAddLine(TransportDB* tdb, const char *type, int line_id, float price);
+TransportResult TransportRemoveLine(TransportDB* tdb, int line_id);
+
 
 #endif
